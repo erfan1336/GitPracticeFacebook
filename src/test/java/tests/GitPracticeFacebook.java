25d030d85c6fdfe31436662ace932763b1d5939a
erfan1336 <58499777+erfan1336@users.noreply.github.com>
2020-03-11 20:56:40 -0700

    Update GitPracticeFacebook.java
    update to test the PULL

--- src/test/java/tests/GitPracticeFacebook.java
+++ src/test/java/tests/GitPracticeFacebook.java
@@ -5,5 +5,9 @@ public class GitPracticeFacebook {
     public static void main(String[] args) {
 
         System.out.println("facebook");
+        
+         System.out.println("this is the update to test the PULL");
+        
+        
     }
 }
